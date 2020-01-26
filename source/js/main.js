ce72ee1935027a2eab1ee9a9e6362bff26855f86
vetneka <vetneka@yandex.ru>
2020-01-26 13:01:38 +0400

    Добавил svg-спрайт, подключил иконки на страницу, внес исправления для ie11

--- source/js/main.js
+++ source/js/main.js
@@ -1,3 +1,73 @@
-'use strict'
+"use strict"
 
-document.createElement( "picture" );
+var mainHeader = document.querySelector(".main-header");
+var mainLogo = document.querySelector(".main-logo");
+var hamburger = document.querySelector(".hamburger");
+
+var countriesFilter = document.querySelector(".countries-filter");
+var countriesFilterButtonOutside = document.querySelector(".countries-filter__button-outside");
+var countriesFilterButtonInside = document.querySelector(".countries-filter__button-inside");
+
+var companionsForm = document.querySelector(".companions-form");
+var companionsFormFieldset = document.querySelectorAll(".companions-form__fieldset");
+var companionsFormLegend = document.querySelectorAll(".companions-form__legend");
+
+var modalTariffs = document.querySelector(".modal--tariffs");
+var modalButton = document.querySelector(".modal__button");
+var tariffsLink = document.querySelector(".tariffs__link");
+
+mainHeader.classList.remove("main-header--no-js");
+mainLogo.classList.remove("main-logo--no-js");
+
+hamburger.addEventListener("click", function () {
+  mainHeader.classList.toggle("main-header--opened");
+  mainLogo.classList.toggle("main-logo--opened");
+  hamburger.classList.toggle("hamburger--opened");
+});
+
+if (countriesFilter || countriesFilterButtonOutside || countriesFilterButtonInside) {
+  countriesFilter.classList.remove("countries-filter--no-js");
+
+  countriesFilterButtonOutside.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    countriesFilter.classList.toggle("countries-filter--close");
+  });
+
+  countriesFilterButtonInside.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    countriesFilter.classList.toggle("countries-filter--close");
+  });
+};
+
+if (companionsForm) {
+  companionsForm.classList.remove("companions-form--no-js");
+
+  for (var i = 0; i < companionsFormLegend.length; i++) {
+    (function (i) {
+      companionsFormFieldset[i].classList.remove("companions-form__fieldset--open");
+      companionsFormLegend[i].addEventListener("click", function(evt) {
+        evt.preventDefault();
+        companionsFormFieldset[i].classList.toggle("companions-form__fieldset--opened");
+        companionsFormFieldset[i].classList.toggle("companions-form__fieldset--open");
+      })
+    })(i);
+  }
+};
+
+if (modalTariffs || tariffsLink) {
+  tariffsLink.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    modalTariffs.classList.add("modal--show");
+  });
+
+  modalButton.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    modalTariffs.classList.remove("modal--show");
+  })
+};
+
+(function () {
+  if("svg4everybody" in window) {
+    svg4everybody();
+  }
+})();
